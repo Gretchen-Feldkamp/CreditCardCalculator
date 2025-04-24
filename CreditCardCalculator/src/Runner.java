a0eb389ac0db7d6191685a65a1e07e3dbf8f9457
gretchen.feldkamp <gretchen.feldkamp@MU-C1-21.mullen.pvt.k12.co.us>
2025-04-24 12:00:23 -0600

    la

--- CreditCardCalculator/src/Runner.java
+++ CreditCardCalculator/src/Runner.java
@@ -11,38 +11,93 @@ public class Runner
 		public static void main(String[] args)
 			{
 				Reader.readerMain();
+				System.out.println("Pleas select option 1 or 2.");
+				System.out.println("1: enter card 2. Scan cards");
+				Scanner userInput=new Scanner(System.in);
+				String answer=userInput.next();
+				if(answer.equals("1")) 
+				{
 				stepsInput();
+				}
+				else if(answer.equals("2"))
+					{
 				stepsReader();
+					}
+				else
+					{
+						System.out.println("Invalid answer");
+						return;
+					}
 			}
 		
-		public static boolean stepsInput()
+		public static void stepsInput()
 		{
 			
-			int total = 0;
+			System.out.println("Please enter card number");
+			
+			Scanner userInput=new Scanner(System.in);
+			
+			total = 0;
+			String userInput2=userInput.nextLine();
+			String[] userInput1=userInput2.split("");
+			int[] digits=new int[userInput2.length()];
+			
+			for(int t=0; t<userInput1.length;t++)
+				{
+			digits[t] = Integer.parseInt(userInput1[t]);
+
+				}
+			
+	        int[] doubleValues = new int[digits.length];
+	        for (int i = 0; i < digits.length; i++) 
+	        {
+	            doubleValues[i] = digits[i];
+	        }
+	        for (int i = 0; i<digits.length-1; i=i+2) 
+	        {
+	            doubleValues[i] *= 2;
+	            if (doubleValues[i] >= 10) 
+	            {
+	                String[] individualparts = Integer.toString(doubleValues[i]).split("");
+	                doubleValues[i] = Integer.parseInt(individualparts[0]) + Integer.parseInt(individualparts[1]);
+	            }
+	        }
+	        for (int num : doubleValues) 
+	        {
+	            total += num;
+	        }
+			
 			
 			 if(total%10==0)
 		        {
-		        	System.out.println("This is a valid number");
-		        	return true;	
+		        	System.out.println("This is a valid number");	
 		        }
 		        else
 		        {
 		        	System.out.println("This is not a valid number");
-		        	return false;
 		        }
 		}
 		
-		public static boolean stepsReader()
+		public static void stepsReader()
 		{
+			int counter=0;
 			while(Reader.cardFile.hasNext())
 			{
+			String cardNum = Reader.cardFile.next();
 			total = 0;
-	        int[] doubleValues = new int[Reader.individualInt.length];
-	        for (int i = 0; i < Reader.individualInt.length; i++) 
+			String[] parts = cardNum.split("");
+			int[] digits = new int[parts.length];
+			for (int i = 0; i < parts.length; i++) 
+			{
+			    digits[i] = Integer.parseInt(parts[i]);
+			}
+			
+	        int[] doubleValues = new int[digits.length];
+	        for (int i = 0; i < digits.length; i++) 
 	        {
-	            doubleValues[i] = Reader.individualInt[i];
+	            doubleValues[i] = digits[i];
 	        }
-	        for (int i = 0; i<Reader.individualInt.length-1; i=i+2) 
+	        for (int i = 0; i<digits.length-1; i=i+2) 
 	        {
 	            doubleValues[i] *= 2;
 	            if (doubleValues[i] >= 10) 
@@ -58,15 +113,15 @@ public class Runner
 
 	        if(total%10==0)
 	        {
-	        	System.out.println(Reader.cardFile.next()+" is a valid number");
-	        	return true;	
+	        	System.out.println(cardNum+" is a valid number");
+	        	counter++;
 	        }
 	        else
 	        {
-	        	System.out.println(Reader.cardFile.next()+" is not a valid number");
-	        	return false;
+	        	System.out.println(cardNum+" is not a valid number");
 	        }
 			}
-			return true;
+			System.out.println(counter + " valid cards are present");
+
 	    }
 	}
